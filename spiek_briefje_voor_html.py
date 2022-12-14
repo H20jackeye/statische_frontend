d70242923458945e41840f34975d910d80bd3031
lucas van eijnsbergen <99073105@mydavinci.nl>
2022-12-14 09:43:38 +0100

    index_html

---
+++ spiek_briefje_voor_html.py
@@ -0,0 +1,75 @@
+# dit is een title
+# </title><head>
+    #<title>_hier zet je de title</title>
+# </head>
+#-----------------------------------------------
+#<body>
+    
+#</body>
+#dit is een body
+#daar zet je dingen tussen zoals
+#<H1> <H2> <H3> <H4> 
+#je kan ook een <p> </p> er tussen zetten dat is een paragraaf
+# je kunt ook een <a href=""></a> gebruiken dit is voor linkjes
+# je kunt images in je html code zetten door <img src="" alt="" width="" height="1">
+#deze code te gebruiken
+#--------------------------------------------------
+#je kunt hier mee een knop op je pagina zetten
+# <button></button>
+#--------------------------------------------------
+#je kunt een ordered list maken en een un ordered, dus met volgorde of zonder
+#dat kan je doen met deze code
+#dit is een unordered list
+#<ul>
+  #<li>Coffee</li>
+  #<li>Tea</li>
+  #<li>Milk</li>
+#</ul>
+#en dit is een ordered list
+#<ol>
+  #<li>Coffee</li>
+  #<li>Tea</li>
+  #<li>Milk</li>
+#</ol> 
+#-------------------------------------------------
+#om je leters dikker te maken gebruik je bij voorbeeld 
+#<p><b>_dan een zin_</b></p>
+#dus gewoon <b> </b> 
+#_______________________________________________
+#om een lijn tussen bepaalde rijen te maken gebruik je:
+#  <hr>
+#dan krijg je zoon mooie lijn#_______________________________________________
+#--------------------------------------------------------------------
+#als je een zin wilt breken in stukjes gebruik je <br>
+#---------------------------------------------------------------------
+#een <pre> tag helpt met alvast spacies en zin breuken er in te zetten:
+#  <pre>
+#---------------------------------------------------------
+#hier mee kan je kleur veranderen en de grote
+#<p>I am normal</p>
+#<p style="color:red;"> I am red </p>
+#<p style="color:blue;"> I am blue </p>
+#<p style="font-size:50px;"> I am big </p>
+#-------------------------------------------------------------
+#<h1 style="font-family:verdana;">This is a heading</h1>
+#<p style="font-family:courier;">This is a paragraph.</p>
+#dit is andere soort font
+#------------------------------------------------------------
+#met dit maak je text groot of klein
+#<h1 style="font-size:400%;">This is a heading</h1>
+#<p style="font-size:160%;">This is a paragraph.</p>
+#---------------------------------------------------------------
+#zo krijg je text in het midden van je pagina
+#<h1 style="text-align:center;">Centered Heading</h1>
+#<p style="text-align:center;">Centered paragraph.</p>
+#---------------------------------------------------------------
+#je kan text ook MARKEN 
+#<h1 style="text-align:center;">Centered Heading</h1>
+#<p style="text-align:center;">Centered paragraph.</p>
+#--------------------------------------------------------------
+#je kan ook COMBINATIES  maken zoals
+#een button en een link 
+#<a href=""><button>klik hier</button></a>
+#-------------------------------------------------------------
+#of een button een link en een cleur zoals dit
+# <button><a href="" style="color:green;">klik hier</a></button>
